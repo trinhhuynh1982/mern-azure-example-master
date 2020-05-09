c60cf1372f73102fa5077103df302c9815dd5f46
thuynh64 <thuynh64@dxc.com>
2020-05-09 11:44:58 +0700

    second coomit

---
+++ client/mern_azure_example/build/precache-manifest.18d53eb216a87a8f74d8b8bcba79cb93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "45a39292dc3b7ac7d496",
+    "url": "/static/js/main.45a39292.chunk.js"
+  },
+  {
+    "revision": "d9dd60602ca8d147855e",
+    "url": "/static/js/1.d9dd6060.chunk.js"
+  },
+  {
+    "revision": "45a39292dc3b7ac7d496",
+    "url": "/static/css/main.d857107b.chunk.css"
+  },
+  {
+    "revision": "5d9eadd91fb555e5755fb0647b1a56d0",
+    "url": "/index.html"
+  }
+];
